0cdd0eb5e4d93181eee2c020a76454d0ac1be5ba
Ty Gooch <gooch.ty@gmail.com>
2018-03-14 20:17:32 -0700

    added google maps view, fixes issue #7

--- src/main/java/iv-emergency-dispatch/IndexController.java
+++ src/main/java/iv-emergency-dispatch/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class IndexController {
 
     @RequestMapping("/")
-    public String greeting() {
+    public String index() {
         return "index";
     }
 
